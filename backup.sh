1d9478756bbc141a05a60afa87c38934f4aef547
Thomas Tran <thomastran878@gmail.com>
2018-11-10 15:24:26 -0800

    Added notification message, backup can be run from anywhere

--- backup.sh
+++ backup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Move into the current directory
+cd "$(dirname "$0")"
+
 while true; do
     read -p "Would you like to backup Xfce files? [y/n]: " yn
     case $yn in
@@ -16,6 +19,9 @@ cp ~/.config/rofi/config ./rofi
 cp ~/torrents/scripts/* ./torrents/scripts
 sed -i "/ExecStart/c\ExecStart=/home/_USER_/torrents/scripts/watchdir.sh" ./torrents/scripts/watchdir.service
 sed -i "/User/c\User=_USER_" ./torrents/scripts/watchdir.service
+
+# Move back
+cd -
 
 cat <<EOF
 ---------------
--- torrents/scripts/remove-torrents.sh
+++ torrents/scripts/remove-torrents.sh
@@ -6,6 +6,9 @@ file=$(/usr/bin/transmission-remote 127.0.0.1:9091 -l | grep 100% | awk '{print 
 # Move queued torrents to completed directory
 mv ~/torrents/tor_queue/$file.torrent ~/torrents/tor_complete
 
+# Send notification
+notify-send -t 5000 "$file has finished downloading"
+
 # Remove torrent using process ID
 /usr/bin/transmission-remote 127.0.0.1:9091 -l | grep 100% | awk '{print $1}' | xargs -n 1 -I % /usr/bin/transmission-remote 127.0.0.1:9091 -t % -r
 
