a45d675d645c0800876944c5080f001729df36b9
Thomas Tran <thomastran878@gmail.com>
2019-03-31 02:23:09 -0700

    Fixed torrent move; Added history substring to zshrc

--- torrents/scripts/watchdir.sh
+++ torrents/scripts/watchdir.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-inotifywait -m ~/Downloads -e create -e moved_to |
-    while read path action file; do
-        # do something with the file
+inotifywait -m ~/Downloads -e create -e moved_to --format '%f' |
+    while read file; do
+        # check if it's a torrent file
         if [ ${file: -8} == ".torrent" ]; then
-            mv ~/Downloads/$file ~/torrents/tor_queue
+            # Give the download some time to create both torrent file and part file
+            sleep 1
+            # if the file is still downloading, wait until it completes
+            while [ -e ~/Downloads/"$file".part ]; do
+                sleep 1
+            done
+            # move the file to the correct location
+            mv ~/Downloads/"$file" ~/torrents/tor_queue
         fi
     done
+
