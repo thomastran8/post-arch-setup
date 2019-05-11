2010f20fddc7a4e45a1cf3dc64d6d210aa485cf4
Thomas Tran <thomastran878@gmail.com>
2019-05-11 00:33:08 -0700

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ overwrite_files()
     cp ./neovim/init.vim ~/.config/nvim/init.vim
     #TODO: overwrite i3 files
     #TODO: overwrite urxvt files
+    #TODO: overwrite mpd /etc location
     mkdir -p ~/.config/rofi
     cp ./rofi/config ~/.config/rofi/config
     mkdir -p ~/remote   # For sshfs
@@ -27,8 +28,8 @@ overwrite_files()
 
 # Overwrite files
 while true; do
-    read -p "Warning: System configuration files will be overwritten
-    Would you like to overwrite files? [y/n]: " yn
+    read -p "Warning: Current system configuration files will be overwritten
+    Would you like to overwrite the files now? [y/n]: " yn
     case $yn in
         [Yy]* ) overwrite_files; break;;
         [Nn]* ) break;;
@@ -44,7 +45,10 @@ sed -i "s/colorscheme/\"colorscheme/g" ~/.config/nvim/init.vim
 while true; do
     read -p "Do you wish to update mirrorlists? [y/n]: " yn
     case $yn in
-        [Yy]* ) sudo rankmirrors -f; break;;
+        [Yy]* ) sudo pacman -S reflector --noconfirm
+		reflector --latest 30 --sort rate --country "United States" --save /etc/pacman.d/mirrorlist
+		break
+		;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -57,13 +61,51 @@ sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
 sudo pacman -Syu --noconfirm \
 zsh \
 neovim \
-xclip \
+intel-ucode \
+xorg \
+intel-ucode \ # intel microcode
+xorg \
+xorg-xinit \
+mesa \ # may be already installed
+vulkan-intel \
+i3 \
+redshift \ # not working
+ttf-dejavu \
+zplug \
+tmux \
+firefox \
+neofetch \
 ranger \
-noto-fonts-emoji \
-rust \
+rofi \
+htop \
+feh \
+imagemagick \
+mpv \
+scrot \
+pulseaudio \ # requires restart
+pamixer \
+pavucontrol \ # optional, nice to have
+pcmanfm \ # optional, nice to have
+pastebinit \
+xdg-user-dirs \
+arandr \
+libreoffice \
+network-manager-applet \ # optional, nice to have
+gtop \
+fuse \ # requires restart
+noto-fonts-emoji \ # refresh i3
+udisks2 \
+udiskie \
+dunst \
+mpd \
+mpc \
+ncmpcpp \
+libmpdclient \ # install before polybar
+xclip \
 transmission-cli \
 inotify-tools \
 sshfs
+#rust \
 
 # Install AUR package manager
 git clone https://aur.archlinux.org/yay.git ~/yay
@@ -75,8 +117,15 @@ rm -rf ~/yay
 # Update YAY and install packages
 yay -Syu --noconfirm \
 zplug \
-nerd-fonts-complete-mono-glyphs \
-neovim-plug-git
+nerd-fonts-dejavu-complete \
+neovim-plug-git \
+systemd-numlockontty \
+polybar \
+cryptomator \
+megasync \
+rxvt-unicode-pixbuf \
+light \
+fontviewer
 
 # Install python modules for neovim
 pip3 install neovim --user
@@ -89,7 +138,6 @@ sed -i "s/\"colorscheme/colorscheme/g" ~/.config/nvim/init.vim
 
 #TODO: reload i3
 
-#TODO: backup mpd /etc location
 
 # Default to zsh
 chsh -s $(which zsh)
